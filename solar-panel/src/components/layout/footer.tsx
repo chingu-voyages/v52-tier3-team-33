8e1d6eb2fb391363dec0988e653635fca71d936a
Emilia Burza <emila.burza@gmail.com>
2025-01-05 20:57:59 +0100

    Create simple footer

---
+++ solar-panel/src/components/layout/footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer(): React.ReactNode {
+  return (
+    <footer className="border-t">
+      <div className="container py-4 text-center text-sm">
+        <p>© 2024 Solar Panel App. All rights reserved.</p>
+      </div>
+    </footer>
+  );
+}
