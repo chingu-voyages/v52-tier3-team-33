146e268455dd6e4c23984004da576eb9acedb755
Emilia Burza <emila.burza@gmail.com>
2025-01-07 13:53:03 +0100

    Add basic main section structure

--- solar-panel/src/app/page.tsx
+++ solar-panel/src/app/page.tsx
@@ -1,10 +1,9 @@
+import Main from "./public/page";
+
 export default function Home(): React.ReactNode {
   return (
-    <main className="row-start-2 flex flex-col items-center gap-8 sm:items-start">
-      <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm sm:text-left">
-        <li>Welcome to Solar Panel App</li>
-        <li>Save and see your changes instantly.</li>
-      </ol>
+    <main className="row-start-2 flex flex-col items-center">
+      <Main />
     </main>
   );
 }
---
+++ solar-panel/src/app/public/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+// import Link from "next/link";
+
+// import { Button } from "@/components/ui/button";
+
+export default function Main(): React.ReactNode {
+  return (
+    <div className="main">
+      <section className="hero main-section">
+        <h1>Welcome to Solar Panel App</h1>
+        <p>Save and see your changes instantly.</p>
+      </section>
+      <section className="secondary-navigationmain-section">
+        <h2>Benefits of Solar Panel</h2>
+      </section>
+      <section className="benefits main-section">
+        <h2>Benefits of Solar Panel</h2>
+        <p>Request an evaluation of your solar panel system.</p>
+      </section>
+      <section className="process main-section">
+        <h2>Process</h2>
+        <p>Request an evaluation of your solar panel system.</p>
+      </section>
+      <section className="faq main-section">
+        <h2>FAQ</h2>
+        <p>Request an evaluation of your solar panel system.</p>
+      </section>
+    </div>
+  );
+}
