68072bf3b2f26c6065f41edc1f465c16c32a4ae1
Emilia Burza <emila.burza@gmail.com>
2025-01-08 20:33:58 +0100

    Change location of main page components

--- solar-panel/src/app/public/page.tsx
+++ solar-panel/src/app/public/page.tsx
@@ -1,10 +1,10 @@
 "use client";
 
-import Benefits from "@/components/layout/sections/benefits";
-import CTA from "@/components/layout/sections/cta";
-import FAQ from "@/components/layout/sections/faq";
-import Hero from "@/components/layout/sections/hero";
-import Process from "@/components/layout/sections/process";
+import Benefits from "@/app/public/adopt/benefits";
+import CTA from "@/app/public/adopt/cta";
+import FAQ from "@/app/public/adopt/faq";
+import Hero from "@/app/public/adopt/hero";
+import Process from "@/app/public/adopt/process";
 // import SectionSecondaryNavigation from "@/components/layout/sections/section-secondary-navigation";
 
 export default function Main(): React.ReactNode {
