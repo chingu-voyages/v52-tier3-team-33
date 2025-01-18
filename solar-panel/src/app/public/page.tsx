9c807f2cc453bf5ec65705cca4ad8e249fba2269
Emilia Burza <emila.burza@gmail.com>
2025-01-18 18:37:40 +0100

    Delete comments in main page

--- solar-panel/src/app/public/page.tsx
+++ solar-panel/src/app/public/page.tsx
@@ -5,14 +5,11 @@ import CTA from "@/app/public/adopt/cta";
 import FAQ from "@/app/public/adopt/faq";
 import Hero from "@/app/public/adopt/hero";
 import Process from "@/app/public/adopt/process";
-// import SectionSecondaryNavigation from "@/components/layout/sections/section-secondary-navigation";
 
 export default function Main(): React.ReactNode {
   return (
     <div className="flex min-h-screen w-full flex-col items-center justify-start">
       <Hero />
-      {/* TODO: Add SectionSecondaryNavigation if needed */}
-      {/* <SectionSecondaryNavigation /> */}
       <Benefits />
       <Process />
       <FAQ />
