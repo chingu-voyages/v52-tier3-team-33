1d4677ecd603a1ad51e9118e7706d092841afc1b
Emilia Burza <emila.burza@gmail.com>
2025-01-18 18:38:29 +0100

    Fix issues with user authentication

--- solar-panel/src/stores/auth-store.tsx
+++ solar-panel/src/stores/auth-store.tsx
@@ -1,31 +1,22 @@
 import type { User } from "@supabase/supabase-js";
 import { create } from "zustand";
 
-import { supabase } from "@/utils/db/client";
 import type { LoginSchemaType } from "@/utils/schemas/auth";
 
 interface AuthState {
   user: User | null;
   isAuthenticated: boolean;
   login: (credentials: LoginSchemaType) => Promise<void>;
-  logout: () => void;
+  logout: () => Promise<void>;
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   user: null,
   isAuthenticated: false,
   login: async (credentials) => {
-    const { data, error } = await supabase.auth.signInWithPassword(credentials);
-
-    if (error) {
-      throw new Error(error.message);
-    }
-
-    set({ user: data.user, isAuthenticated: true });
+    set({ isAuthenticated: true, user: { email: credentials.email } as User });
   },
-  logout: () =>
-    set({
-      user: null,
-      isAuthenticated: false,
-    }),
+  logout: async () => {
+    set({ user: null, isAuthenticated: false });
+  },
 }));
