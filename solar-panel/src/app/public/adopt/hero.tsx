68b0af8d2306709b87606bc2407ff650888c9b39
Emilia Burza <emila.burza@gmail.com>
2025-01-23 17:30:10 +0100

    Add styling to hero component

--- solar-panel/src/app/public/adopt/hero.tsx
+++ solar-panel/src/app/public/adopt/hero.tsx
@@ -4,13 +4,13 @@ import { Button } from "@/components/ui/button";
 
 export default function Hero(): React.ReactNode {
   return (
-    <div className="flex max-h-96 min-h-80 w-full flex-col items-center justify-center bg-[url('../../public/img/hero-img.png')] bg-cover bg-center">
-      <h1>Free Solar Panel Evaluation</h1>
-      <p>
+    <div className="flex min-h-[32rem] w-full flex-col items-center justify-center bg-[url('../../public/img/hero-img.png')] bg-cover bg-center text-white">
+      <h1 className="text-4xl font-bold">Free Solar Panel Evaluation</h1>
+      <p className="mt-4 max-w-2xl text-center text-lg">
         Get a professional assessment of your property&apos;s solar potential
         from city-approved specialists
       </p>
-      <Link href="/public/evaluate">
+      <Link href="/public/evaluate" className="mt-8">
         <Button variant="default" size="lg">
           Request an Evaluation
         </Button>
